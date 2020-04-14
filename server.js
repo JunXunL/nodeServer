53ca66e057548fd64d999b26b736336b3c252d28
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-04-14 11:21:20 +0800

    feat: 增加练习手札

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
-//引用node的http模块
-var http = require('http');
+// 直接创建第一个http server
 
+var PORT = 8080; //端口
+var DIR = 'views/testServer'; //用于存放html的目录
+
+var http = require('http');//引用node的http模块
+var url=require('url');
+var fs=require('fs');
+var contentTypeModule=require('./contentType').types;
+var path=require('path');
 //创建一个服务器并指定请求处理函数
-http.createServer(function(req,res){
-  //设置返回的请求状态 200位成功 和返回头部及文件编码
-  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  //向客户端返回内容
-  res.end('Hello world ！ 我是node服务器的返回！');
-}).listen(8888); //监听 8888 端口
-console.log('服务器开启在：http://localhost:8888/');
+var server = http.createServer(function (request, response) {
+  var pathname = url.parse(request.url).pathname;
+  var realPath = path.join(DIR, pathname);
+  //console.log(realPath);
+  var ext = path.extname(realPath);
+  ext = ext ? ext.slice(1) : 'unknown';
+  fs.exists(realPath, function (exists) {
+    if (!exists) {
+      //设置返回的请求状态 200位成功 和返回头部及文件编码
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.write("This request URL " + pathname + " was not found on this server.");
+        response.end(); //向客户端返回内容
+    } else {
+      fs.readFile(realPath, "binary", function (err, file) {
+        if (err) {
+          response.writeHead(500, {'Content-Type': 'text/plain'});
+          response.end(err);
+        } else {
+          var contentType = contentTypeModule[ext] || "text/plain";
+          response.writeHead(200, {'Content-Type': contentType});
+          response.write(file, "binary");
+          response.end();
+        }
+      });
+    }
+  });
+});
+server.listen(PORT);
+// http://localhost:8080/testServer.html
+console.log("Server runing at port: " + PORT + ".");
