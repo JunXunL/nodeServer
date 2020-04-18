4bfb46eb607b183e0a7b234f1759cb9cfaa1bc3c
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-04-19 00:48:27 +0800

    feat: 路由配置 - ajax请求

---
+++ routes/ajaxDemo.js
@@ -0,0 +1,30 @@
+// Ajax 请求练习
+const express = require('express')
+const router = express.Router()
+
+// ajaxDemo-post的加载页面
+// 项目启动，http://localhost:3000/ajaxDemo/ajaxDemo-post，打开views下的ajaxDemo-post.html页面
+router.get('/ajaxDemo-post', function(req, res){ // 设置路由名称
+  res.render('ajaxDemo-post'); // 加载模板文件
+})
+// 检查答案的路由
+router.post('/checkAnswer', function(req,res){
+  // 利用cors解决跨越问题
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  if(req.body.answer1 != "人生" && req.body.answer2 != '丹心'){
+    res.send('错误！');
+  }else{
+    res.send('正确！');
+  }
+});
+// ajaxDemo-get的加载页面
+router.get('/ajaxDemo-get',function(req,res){
+  res.render('/ajaxDemo-get');
+});
+//查看答案的路由
+router.get('/lookAnswer',function(req,res){
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.json({answer1:'人生',answer2:'丹心'});
+});
+
+module.exports = router
