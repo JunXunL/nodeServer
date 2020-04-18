cf052670c7f141acd70ffba9e45912e17a153208
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-04-19 00:49:12 +0800

    feat: 路由分离练习，默认首页

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET views/index.html */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '首页' }); //  项目启动，默认http://localhost:3000 访问views/index.html，测试路由配置成功
 });
 
 module.exports = router;
