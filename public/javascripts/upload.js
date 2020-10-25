14ff78d1bacbddf994a93545be6cc4be7def120c
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-10-26 00:35:00 +0800

    feat: 实现ajax原生方式创建文件目录及上传文件

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -2,3 +2,19 @@ function uploadImages() {
   console.log("开始上传图片--------------")
   ajaxGet("/fileUpload/upload")
 }
+
+function newPath() {
+  const pathStr = document.getElementById("path").value.trim();
+  console.log(pathStr)
+  ajaxPost("/fileUpload/newPath", pathStr, fn);
+  function fn(res) {
+    const result = JSON.parse(res)
+    if ((result.status == "1") && (result.content != null)) {
+      document.getElementById("path").setAttribute("disabled", "disabled");
+      document.getElementById("path-submit").style.display = "none";
+      document.getElementById("newPath").value = result.content.path;
+    } else {
+      document.getElementById("path-error").style.display = "block";
+    }
+  }
+}
