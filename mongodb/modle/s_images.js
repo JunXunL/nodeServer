9381065ec4b64d1c3d80f0ddbf99a171b04a74d7
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-10-26 00:33:41 +0800

    feat: 创建图片表

--- mongodb/modle/s_images.js
+++ mongodb/modle/s_images.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const shame = new mongoose.Schema({
+const shema = new mongoose.Schema({
+  s_path: {type: String, require: true}, // 存放目录层级
   s_url: {type: String, require: true}, // 绝对路径
-  s_type: {type: String, require: true} // 图片用于什么模块
+  s_type: {type: String, require: true}, // 图片用于什么模块
+  s_title: {type: String, require: true} // 图片标题
 })
 
-const Image = mongoose.model('s_images', shame)
+const Image = mongoose.model('s_images', shema)
 
 module.exports = Image
