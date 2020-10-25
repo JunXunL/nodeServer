e344c0ac4e735fce1ab82ec8ac529a8b696b56bb
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2020-10-26 00:32:23 +0800

    feat: nodejs模块(暴露接口和引入模块)

--- public/javascripts/isEmpty.js
+++ public/javascripts/isEmpty.js
@@ -7,7 +7,21 @@ function isEmpty(value) {
   } else if (Object.prototype.toString.call(value) == `[object Object]`) {
     return JSON.stringify(value) == "{}" || Object.keys(value).length == 0;
   } else {
-    console.log("isEmpty 方法只能判断Object、String、Array类型是否为空！")
+    console.log("isEmpty 方法只能判断Object、String、Array、XMLHttpRequest类型是否为空！")
     return false;
   }
+}
+
+function isFun(fn) {
+  return Object.prototype.toString.call(fn)==='[object Function]'
+}
+
+function isXMLHttpRequest(obj) {
+  return Object.prototype.toString.call(obj) === `[object XMLHttpRequest]`
+}
+
+module.exports = {
+  isEmpty: isEmpty,
+  isFun: isFun,
+  isXMLHttpRequest: isXMLHttpRequest
 }
