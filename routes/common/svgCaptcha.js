2f9f9550bc92ed618693d17cd2d74a89f6de890d
Yalin.Zhang <Yalin.Zhang@connext.com.cn>
2021-02-16 03:52:39 +0800

    fix: 生产动态密码，添加备注

--- routes/common/svgCaptcha.js
+++ routes/common/svgCaptcha.js
@@ -2,13 +2,14 @@
  * @Descripttion: svg-captcha验证码，使用方法
  * @Author: Irene.Z
  * @Date: 2021-02-14 01:31:58
- * @LastEditTime: 2021-02-14 01:42:42
- * @FilePath: \nodeServer\public\javascripts\svgCaptcha.js
+ * @LastEditTime: 2021-02-16 01:17:02
+ * @FilePath: \nodeServer\routes\common\svgCaptcha.js
  */
 // 进行测试发现没有返回值，排查错误后发现是没有注册session中间件，svg-captcha依赖session存储验证码信息！！！
 const express = require('express');
-const svgCaptcha = require('svg-captcha');
 const router = express.Router();
+const svgCaptcha = require('svg-captcha');
+
 router.get('/',(req, res)=>{
   const cap = svgCaptcha.create({
     // 翻转颜色
@@ -22,8 +23,16 @@ router.get('/',(req, res)=>{
     // 高度
     height: 30,
   });
+  /**
+   * 用后端产生动态安全码之后可以立即缓存起来，同时传输给前端（这个过程可以前端发起请求）。
+   * 前端按上述步骤对用户密码进行加密，将数据送回后端。
+   * 后端检查 secure_key 在缓存中，取出来计算用于验证的 hmac_result（my_password和secure_key，计算HMAC，得出hmac_result），同时从缓存中删除 secure_key；
+   * 如果 secure_key 不在缓存中，直接拒绝验证。
+   * 
+   * HMAC是密钥相关的哈希运算消息认证码（Hash-based Message Authentication Code）的缩写。
+   */
   req.session.captcha = cap.text; // session 存储验证码数值
-  console.log(req.session)
+  console.log(req.session);
   res.type('svg'); // 响应的类型
   res.send(cap.data)
 })
